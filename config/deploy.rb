f8180650f17f82ca912f2908d8e62f0fa76b63d6
Mike Campbell <mike.campbell@meritec.co.uk>
2017-04-20 21:00:09 +0100

    Add secrets.yml to cap linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :repo_url, 'git@github.com:mikecmpbll/alhg.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
